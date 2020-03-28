2d80a3364b921bda608add4c0184440a94d8d6f9
ShankS3 <shashank.surwase@gmail.com>
2020-03-29 01:26:37 +0530

    Updated list reducer and initialState as per actions

--- src/reducers/listReducer.js
+++ src/reducers/listReducer.js
@@ -1,8 +1,10 @@
 import {
-  ON_FETCH_TESTAMENTS,
-  ON_FETCH_BOOKS,
-  ON_FETCH_CHAPTERS,
-  ON_FETCH_TITLES,
+  ON_FETCH_TESTAMENTS_SUCCESS,
+  ON_FETCH_TESTAMENTS_FAILURE,
+  ON_FETCH_BOOKS_SUCCESS,
+  ON_FETCH_BOOKS_FAILURE,
+  ON_FETCH_CHAPTERS_SUCCESS,
+  ON_FETCH_CHAPTERS_FAILURE,
   ON_FETCH_VERSES,
 } from '../constants/actionTypes';
 import initialState from './initialState';
@@ -10,14 +12,54 @@ import objectAssign from 'object-assign';
 
 function listReducer(state = initialState.list, action) {
   switch (action.type) {
-    case ON_FETCH_TESTAMENTS:
-      return objectAssign({}, state, {testaments: action.testaments});
-    case ON_FETCH_BOOKS:
-      return objectAssign({}, state, {books: action.books});
-    case ON_FETCH_CHAPTERS:
-      return objectAssign({}, state, {chapters: action.chapters});
-    case ON_FETCH_TITLES:
-      return objectAssign({}, state, {titles: action.titles});
+    case ON_FETCH_TESTAMENTS_SUCCESS:
+      return objectAssign({}, state, {
+        testaments: objectAssign({}, state.testaments, {
+          list: action.response,
+          error: false,
+          errorMessage: ""
+        })
+      });
+    case ON_FETCH_TESTAMENTS_FAILURE:
+      return objectAssign({}, state, {
+        testaments: objectAssign({}, state.testaments, {
+          list: [],
+          error: true,
+          errorMessage: action.response
+        })
+      });
+    case ON_FETCH_BOOKS_SUCCESS:
+      return objectAssign({}, state, {
+        books: objectAssign({}, state.books, {
+          list: action.response,
+          error: false,
+          errorMessage: ""
+        })
+      });
+    case ON_FETCH_BOOKS_FAILURE:
+      return objectAssign({}, state, {
+        books: objectAssign({}, state.books, {
+          list: [],
+          error: true,
+          errorMessage: action.response
+        })
+      });
+    case ON_FETCH_CHAPTERS_SUCCESS:
+      return objectAssign({}, state, {
+        chapters: objectAssign({}, state.chapters, {
+          list: action.response,
+          error: false,
+          errorMessage: ""
+        })
+      });
+    case ON_FETCH_CHAPTERS_FAILURE:
+      return objectAssign({}, state, {
+        chapters: objectAssign({}, state.chapters, {
+          list: [],
+          error: true,
+          errorMessage: action.response
+        })
+      });
     case ON_FETCH_VERSES:
       return objectAssign({}, state, {verses: action.verses});
     default:
