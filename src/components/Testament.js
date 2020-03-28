cdfc75280f2278723fc840ffac85f6b1f92714c3
ShankS3 <shashank.surwase@gmail.com>
2020-03-29 01:27:06 +0530

    Updated screens

--- src/components/Testament.js
+++ src/components/Testament.js
@@ -1,36 +1,104 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import {View, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  View,
+  Text,
+} from 'react-native';
 
 import ListContainer from './common/ListContainer';
 
-function Testament({books, selectBook, navigation}) {
-  const onItemPress = (itemId) => {
-    selectBook(itemId);
+function Testament({ books, navigation, selectedTestament, bookActions }) {
+
+  useEffect(() => {
+    bookActions.onFetchBooks(selectedTestament.id);
+  }, []);
+
+  const onItemPress = (item) => {
+    bookActions.onSelectBook(item);
     navigation.push("Book");
   };
 
   return (
-    <View style={styles.parent}>
-      <ListContainer
-        title="Old Testament"
-        list={books}
-        onClickEvent={onItemPress}
-      />
-    </View>
+    <SafeAreaView style={styles.parent}>
+      <View style={styles.header} >
+        <Image
+          source={
+            {uri: 'data:image/png;base64,'+selectedTestament.header_img}
+          }
+          style={styles.headerImg} 
+        />
+      </View>
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Introduction</Text>
+        <Text style={styles.text}>
+          {selectedTestament.summary}
+        </Text>
+      </View>
+      <View style={{...styles.card, ...styles.bottomCard}}>
+        <Text style={styles.cardTitle}>Books</Text>
+        <ListContainer
+          list={books.list}
+          onClickEvent={onItemPress}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   parent: {
-    backgroundColor: '#001',
+    display: 'flex',
     height: '100%',
+    backgroundColor: '#ececec',
   },
+  header: {
+    width: '100%',
+    height: 250,
+    borderBottomLeftRadius: 40,
+    overflow: "hidden",
+    elevation: 10
+  },
+  headerImg: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  card: {
+    display: 'flex',
+    width: '100%',
+    minHeight: 50,
+    backgroundColor: '#fff',
+    padding: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    elevation: 5
+  },
+  bottomCard: {
+    alignSelf: 'flex-end',
+    flexGrow: 1,
+    flexShrink: 1
+  },
+  cardTitle: {
+    fontSize: 21,
+    fontWeight: '700',
+    lineHeight: 32,
+    color: '#8f8f8f'
+  },
+  text: {
+    fontSize: 14,
+    lineHeight: 21,
+    fontWeight: '600',
+    color: '#303030'
+  }
 });
 
 Testament.propTypes = {
-  books: PropTypes.array.isRequired,
-  selectBook: PropTypes.func.isRequired,
+  selectedTestament   : PropTypes.object.isRequired,
+  books               : PropTypes.object.isRequired,
+  bookActions         : PropTypes.object.isRequired,
 }
 
 export default Testament;
